9b2a54b274c91be875f455b5ed833cc326d2dd20
Your Adil <khushnood11@gmail.com>
2025-07-08 15:35:32 +0200

    name added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Khushnood Rafique
         </a>
       </header>
       <p>
